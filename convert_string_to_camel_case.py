68cb8d6da0a2adaff2cbad5417c26f9fe5248afc
sparemeyoursoliloquy <35121827+Iglehart@users.noreply.github.com>
2020-08-01 19:14:40 +0800

    Create convert_string_to_camel_case.py

---
+++ convert_string_to_camel_case.py
@@ -0,0 +1,13 @@
+def to_camel_case(text):
+    for i in ['_', '-']:
+        text = text.replace(i,' ')
+    text = text.title()
+    for i in [' ']:
+        text = text.replace(i, '')
+    print(text)
+
+def main():
+    user_input = input('Enter text: ')
+    to_camel_case(user_input)
+
+main()
