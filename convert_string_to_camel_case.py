25301bd2b72b55a98e9fac73a327e0301302d2fd
sparemeyoursoliloquy <35121827+Iglehart@users.noreply.github.com>
2020-08-01 19:20:52 +0800

    Update convert_string_to_camel_case.py

--- convert_string_to_camel_case.py
+++ convert_string_to_camel_case.py
@@ -1,7 +1,7 @@
 def to_camel_case(text):
     for i in ['_', '-']:
         text = text.replace(i,' ')
-    text = text.title()
+    text = text.title() # must be changed
     for i in [' ']:
         text = text.replace(i, '')
     print(text)
